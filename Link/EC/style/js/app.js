cfb1c60d67866e8ce3323d71a2ffe3e59d891aa1
Jimmy2020-web <76805098+Jimmy2020-web@users.noreply.github.com>
2023-02-07 21:05:23 +0600

    ecFrom
    Data to Google

--- Link/EC/style/js/app.js
+++ Link/EC/style/js/app.js
@@ -73,3 +73,13 @@ clearBtn.addEventListener('click', () =>{
     inputs.forEach(input => input.value = "");
     window.location.reload(true);
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby1qALlcYiZOoo-wYbrByvflbPiAdCOblYLmRvyASOkjtGluFQM0_1dndjQhe_SszB6/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
